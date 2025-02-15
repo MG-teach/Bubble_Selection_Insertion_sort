2464d169bc9d28a967889065ecdda62907893238
Emil Petkov <studio.vodopad@gmail.com>
2025-02-15 18:57:22 +0200

    Comments

--- Overview/InsertionSort.cs
+++ Overview/InsertionSort.cs
@@ -15,6 +15,9 @@ namespace Overview
             {
                 int key = array[i];
                 int j = i - 1;
+                /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
                 while (j >= 0 && array[j] > key)
                 {
                     array[j + 1] = array[j];
