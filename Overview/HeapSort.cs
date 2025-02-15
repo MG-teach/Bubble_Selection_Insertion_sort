e9277175051689d9e341a02368c0fd1f49d672db
Emil Petkov <studio.vodopad@gmail.com>
2025-02-15 15:23:14 +0200

    Merge + Quick + Heap sort

---
+++ Overview/HeapSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overview
+{
+    public class HeapSort : SortAlgorithm
+    {
+        public override void Sort(int[] array)
+        {
+            int n = array.Length;
+            for (int i = n / 2 - 1; i >= 0; i--)
+                Heapify(array, n, i);
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                (array[0], array[i]) = (array[i], array[0]);
+                Heapify(array, i, 0);
+            }
+        }
+
+        private void Heapify(int[] array, int n, int i)
+        {
+            int largest = i, left = 2 * i + 1, right = 2 * i + 2;
+
+            if (left < n && array[left] > array[largest]) largest = left;
+            if (right < n && array[right] > array[largest]) largest = right;
+
+            if (largest != i)
+            {
+                (array[i], array[largest]) = (array[largest], array[i]);
+                Heapify(array, n, largest);
+            }
+        }
+    }
+}
